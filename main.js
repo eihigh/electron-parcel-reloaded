b7ea6ef2985bfd2b6578286c63a5b39c0aea592c
eihigh <eihigh.contact@gmail.com>
2020-08-23 23:50:16 +0900

    initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+require("electron-reload")(path.join(__dirname, "dist"));
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadFile(path.join(__dirname, "dist/index.html"));
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.on("closed", function () {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.on("activate", function () {
+  if (mainWindow === null) createWindow();
+});
